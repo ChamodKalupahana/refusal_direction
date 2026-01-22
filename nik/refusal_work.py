2a792a275421dd802b9aa43e5810018db326a8d7
root <chamodkalupahana@gmail.com>
2026-01-22 23:17:08 +0000

    moved to nik folder

--- nik/refusal_work.py
+++ nik/refusal_work.py
@@ -7,7 +7,7 @@ from typing import Dict, List
 import matplotlib.pyplot as plt
 import torch
 
-REPO_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+REPO_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 if REPO_ROOT not in sys.path:
     sys.path.insert(0, REPO_ROOT)
 
